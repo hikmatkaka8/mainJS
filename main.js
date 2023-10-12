0bd5d83df61dbe7b19189a0c82e8d7384eb6a1d4
hikmatkaka8 <hikmatkaka8@gmail.com>
2023-10-12 17:20:25 +0700

    hikmat

---
+++ main.js
@@ -0,0 +1,58 @@
+// console.log("Hello World")
+// alert("ada notifikasi")
+// prompt("Minjam seratus")
+
+// var promnet = "Coding is easy"
+// console.log(promnet)
+// var promnet = "Coding not easy"
+// console.log(promnet)
+
+// console.log(promnet)
+// var promnet = "nothing"
+// console.log(promnet)
+
+// let promnet = "Coding is fun"
+// console.log(promnet)
+// let promnet = "Coding not fun"
+// console.log(promnet)
+
+// const promnet = "Coding yes"
+// console.log(promnet);
+// const promnet = "Coding no"
+// console.log(promnet);
+
+// let totalpoin = prompt("Masukkan poin anda")
+//     if (totalpoin > 100){
+//         document.write("Selamat");
+//     }
+//     else{
+//         document.write("coba lagi")
+//     }
+
+// x = 6
+// y = 2
+// console.log(x<10 && y > 1)
+// console.log(x < 10 && y < 1)
+// console.log(x==5 ||y==5)
+// console.log(x==6 ||y==5)
+// console.log(!(x==y))
+
+// document.write(x<10 && y > 1)
+// document.write("<br>")
+// document.write(x < 10 && y < 1)
+// document.write("<br>")
+// document.write(x==5 ||y==5)
+// document.write("<br>")
+// document.write(x==6 ||y==5)
+// document.write("<br>")
+// document.write(!(x==y))
+
+let p = document.querySelector("p")
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+button.addEventListener('click', function()
+{
+    let isi = input.value
+    console.log(isi)
+    p.innerHTML = isi
+})
